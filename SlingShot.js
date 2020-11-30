89bd9faa9cb420dd1ea852eb04ba84990033d241
Samhitha24 <samhitha.iyengar@gmail.com>
2020-11-30 21:26:47 +0530

    stiffness changed

--- SlingShot.js
+++ SlingShot.js
@@ -3,7 +3,7 @@ class Slingshot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.06,
             length: 1
         }
         this.sling = Constraint.create(options);
